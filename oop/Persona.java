f64a81d63ae40c9075ac7c82fd63acac13bf121d
Edgar <dezcrox@gmail.com>
2023-05-22 16:07:01 -0600

    Se agrego POO

---
+++ oop/Persona.java
@@ -0,0 +1,15 @@
+//definicion de la clase
+class Persona {
+    //atributos - caracteristicas
+    String nombre;
+    int edad;
+    char genero;
+
+    //metodos - acciones
+    void imprimirInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Edad "+edad);
+        System.out.println("Genero "+genero);
+    }
+
+}
