68b7c0bbb693b6c42b587b76fc0b9465fca296cd
Edgar <dezcrox@gmail.com>
2023-05-24 22:38:42 -0600

    Se modificaron los archivos de oop

--- oop/Persona.java
+++ oop/Persona.java
@@ -12,4 +12,18 @@ class Persona {
         System.out.println("Genero "+genero);
     }
 
+    public static void main(String args[]){
+        Persona p=new Persona();
+        p.nombre = "Alex";
+        p.edad = 32;
+        p.genero = 'M';
+        p.imprimirInformacion();
+
+        Persona p1=new Persona();
+        p1.nombre="Juan";
+        p1.edad=51;
+        p1.genero='M';
+        p1.imprimirInformacion();
+    }
+
 }
